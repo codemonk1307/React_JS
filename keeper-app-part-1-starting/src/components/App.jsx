d1f5a20b6f01c01371811b8615672ecc65805193
codemonk1307 <sachinmishra030@gmail.com>
2021-09-01 16:11:15 +0530

    keep-app-part-2-map-components

--- keeper-app-part-1-starting/src/components/App.jsx
+++ keeper-app-part-1-starting/src/components/App.jsx
@@ -1,14 +1,28 @@
 
 import React from "react";
 import Header from "./Header";
-import Notes from "./Notes";
+import Note from "./Note";
 import Footer from "./Footer";
+import notes from "../notes";
+import { func } from "assert-plus";
+import { not } from "micromatch";
+
+
+function createNotes(noteItem){
+    return <Note
+        key = {noteItem.id}
+        title = {noteItem.title}
+        task = {noteItem.task}
+        deadline = {noteItem.deadline}
+    />
+}
+
 
 function App() {
     return (
     <div>
         <Header />
-        <Notes />
+        {notes.map(createNotes)}
         <Footer />
     </div>
     );
