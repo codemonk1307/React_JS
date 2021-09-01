f68fad20cb21ebb295752b704e491e0e9dffbd2f
codemonk1307 <sachinmishra030@gmail.com>
2021-09-01 17:21:58 +0530

    keeper-app-working-version

---
+++ keeper-app/src/notes.js
@@ -0,0 +1,53 @@
+
+const notes = [
+    {
+        id: 1,
+        title: "My Sideul",
+        task: "Today I'll phinish React JS :|",
+        deadline: "10 hours"
+    },
+    {
+        id: 2,
+        title: "My Task",
+        task: "Today I'll do 10 Leetcodes :)",
+        deadline: "10 hours"
+    },
+    {
+        id: 3,
+        title: "My Goal",
+        task: "Today I'll learn Recursion :(",
+        deadline: "10 hours"
+    },
+    {
+        id: 4,
+        title: "Backtracking",
+        task: "Learn backtracking broo u should have decent knowledge about recursion first",
+        deadline: "1 week"
+    },
+    {
+        id: 5,
+        title: "To-Do List App",
+        task: "Make it user specific and quite work needed for making it responsive",
+        deadline: "2 weeks"
+    },
+    {
+        id: 6,
+        title: "Got ideas about TNP App",
+        task: "working on a app which will ease the process of application for students",
+        deadline: "1 month"
+    },
+    {
+        id: 7,
+        title: "Got ideas about TNP App",
+        task: "working on a app which will ease the process of application for students",
+        deadline: "1 month"
+    },
+    {
+        id: 8,
+        title: "Got ideas about TNP App",
+        task: "working on a app which will ease the process of application for students",
+        deadline: "1 month"
+    }
+]
+
+export default notes;
